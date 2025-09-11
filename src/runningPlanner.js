5103a5db39dd5a6c026d2aa038df8560a9413083
Linpursael <vnfmsdl0421@gmail.com>
2025-09-11 17:18:13 +0900

    러닝주차별계획 컴포넌트 생성

---
+++ src/runningPlanner.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
+
+import './runningPlanner.css';
+
+// npm install react-calendar
+
+export default function RunningPlanner() {
+    return (
+        <h1> 하지만 공사중이죠?</h1>
+    );
+}
