5086cd3240cfa27ce8229c40a7efabc72baa6807
Linpursael <vnfmsdl0421@gmail.com>
2025-09-15 17:56:16 +0900

    러닝 주차별 계획 뼈대 만들기

--- src/runningPlanner.js
+++ src/runningPlanner.js
@@ -6,7 +6,45 @@ import './runningPlanner.css';
 // npm install react-calendar
 
 export default function RunningPlanner() {
+    const [selectedWeek, setSelectedWeek] = useState(null);
+    const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth()+1);
+    const [plans, setPlans] = useState({});
+
+    const getWeekNumber = (date) => {
+        const firstDay = new Date(date.getFullYear(), 0, 1);
+        const pastDay = (date-firstDay) /86400000;
+        return Math.ceil((pastDay + firstDay.getDay()+1)/7);
+    };
+        
+    const handleClickDay = (date) => {
+        setSelectedWeek(getWeekNumber(date));
+        setSelectedMonth(date.getMonth() + 1);
+    };
+
+    const addPlan = (plan) => {
+        const key = `${selectedMonth}-${selectedWeek}`;
+        setPlans((prev) => ({
+            ...prev,
+            [key] : [...(prev[selectedWeek,selectedMonth] || []), plan],
+        }));
+
+    };
     return (
-        <h1> 하지만 공사중이죠?</h1>
+       // header 컴포넌트를 따로 만들어 렌더링 예정
+       
+       <div className="mainContainer">
+            <Calendar onClickDay={handleClickDay} />
+            {selectedWeek && selectedMonth && (
+                <div className="calendarContainer">
+                    <h2> {selectedMonth}월 {selectedWeek} 주차 계획 </h2>
+                    <ul>
+                        {(plans[selectedWeek,selectedMonth] || []). map((p,idx) =>(
+                            <li key = {idx}> {p} </li>
+                        ))}
+                    </ul>
+                    <button onClick={()=> addPlan("러닝 5km")}> 일정 추가 </button>
+                </div>
+                )}
+            </div>
     );
 }
