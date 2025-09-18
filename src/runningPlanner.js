7221ef8bf02ee77719512c00d05dfcad5c2a0b37
Linpursael <vnfmsdl0421@gmail.com>
2025-09-18 19:42:50 +0900

    러닝 주차별 계획에 맞게 구조 자체를 변경

--- src/runningPlanner.js
+++ src/runningPlanner.js
@@ -8,43 +8,85 @@ import './runningPlanner.css';
 export default function RunningPlanner() {
     const [selectedWeek, setSelectedWeek] = useState(null);
     const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth()+1);
-    const [plans, setPlans] = useState({});
+    const [weeklyPlans, setWeeklyPlans] = useState({});
+    const [dailyPlans, setDailyPlans] = useState({});
+    const [newWeeklyPlan, setNewWeeklyPlan] = useState("");
+    const [newDailyPlan, setNewDailyPlan] = useState("");
+    //const [plans, setPlans] = useState({});
 
+    /* 기존 1월부터 12월까지의 주차 계산하던 함수 공부를 위해 남겨놓기
     const getWeekNumber = (date) => {
         const firstDay = new Date(date.getFullYear(), 0, 1);
         const pastDay = (date-firstDay) /86400000;
         return Math.ceil((pastDay + firstDay.getDay()+1)/7);
     };
-        
+    */
+    const getWeekNumber = (date) => {
+        const day = date.getDate();
+        return Math.ceil(day/7);
+    } 
+
     const handleClickDay = (date) => {
         setSelectedWeek(getWeekNumber(date));
         setSelectedMonth(date.getMonth() + 1);
     };
 
-    const addPlan = (plan) => {
+    const addWeeklyPlan =() => {
         const key = `${selectedMonth}-${selectedWeek}`;
-        setPlans((prev) => ({
+        if(!newWeeklyPlan) return;
+        setWeeklyPlans((prev) => ({
             ...prev,
-            [key] : [...(prev[selectedWeek,selectedMonth] || []), plan],
+            [key] : [...(prev[key] || []), newWeeklyPlan],
         }));
-
+        setNewWeeklyPlan("");
     };
+
+    const addDailyPlan = (date) => {
+        const key = date.toDateString();
+        if(!newDailyPlan) return;
+        setDailyPlans((prev) => ({
+            ...prev,
+            [key] : [...(prev[key] || []), newDailyPlan],
+        }));
+        setNewDailyPlan("");
+    };
+
+    const tileClassName = ({date}) => {
+        const key = date.toDateString();
+        return dailyPlans[key] ? "has-plan" : null;
+    };
+
     return (
        // header 컴포넌트를 따로 만들어 렌더링 예정
        
        <div className="mainContainer">
-            <Calendar onClickDay={handleClickDay} />
-            {selectedWeek && selectedMonth && (
-                <div className="calendarContainer">
-                    <h2> {selectedMonth}월 {selectedWeek} 주차 계획 </h2>
-                    <ul>
-                        {(plans[selectedWeek,selectedMonth] || []). map((p,idx) =>(
-                            <li key = {idx}> {p} </li>
-                        ))}
-                    </ul>
-                    <button onClick={()=> addPlan("러닝 5km")}> 일정 추가 </button>
-                </div>
-                )}
+            <div className="weeklyPlanForm">
+                <input value={newWeeklyPlan} 
+                onChange={(e) => setNewWeeklyPlan(e.target.value)}
+                placeholder = "이번 주 훈련 계획 입력" />
+                <button onClick={addWeeklyPlan}> 추가 </button>
             </div>
+            <div className="weeklyPlanList">
+                <h3> {selectedMonth} 월 {selectedWeek} 주차 러닝계획 </h3>
+                <ul>
+                    {(weeklyPlans[`${selectedMonth}-${selectedWeek}`] || []). map((plan,idx) =>(
+                        <li key = {idx}> {plan} </li>
+                    )
+                    )}
+                </ul>
+            </div>
+
+                <Calendar onClickDay={handleClickDay} tileClassName={tileClassName} />
+
+                {selectedWeek && selectedMonth && (
+                    <div className="dailyPlanForm">
+                    <input value={newDailyPlan}
+                    onChange={(e) => setNewDailyPlan(e.target.value)}
+                    placeholder="오늘 훈련 입력" />
+                    <button onClick={() => addDailyPlan(new Date())}>추가</button>
+                    </div>
+                 )}
+                 </div>
+                
     );
 }
